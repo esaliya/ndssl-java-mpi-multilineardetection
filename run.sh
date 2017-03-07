23ae7a13e4645c5993ae89f01e1ccdd2d6dcbbbe
Saliya Ekanayake <esaliya@gmail.com>
2017-03-07 17:37:16 -0500

    adding run script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export TMPDIR=/tmp
+
+
+mpirun -np $1 java -cp target/multilineardetection-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.ndssl.multilinearscan.mpi.Program -v 1936 -k 3 -i src/main/resources/2014-01-01_simple.txt -nc 1 -e 0.1
+#mpirun -np $1 java -cp target/multilineardetection-1.0-SNAPSHOT-jar-with-dependencies.jar  org.saliya.ndssl.multilinearscan.mpi.Program -v 7 -k 3 -i src/main/resources/my_little_graph_simple.txt -nc 1 -e 0.1
