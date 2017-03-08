7408cba533829ef55fdc1110d20f886ce545a252
Saliya Ekanayake <esaliya@gmail.com>
2017-03-07 19:32:18 -0500

    All good!

--- src/main/java/org/saliya/ndssl/multilinearscan/mpi/Program.java
+++ src/main/java/org/saliya/ndssl/multilinearscan/mpi/Program.java
@@ -137,6 +137,7 @@ public class Program {
         putils = new ParallelUtils(0, ParallelOps.worldProcRank);
 
         putils.printMessage("\n== " + Constants.PROGRAM_NAME + " run started on " + new Date() + " ==\n");
+        putils.printMessage(configPrettyString(true));
         // get number of iterations for a target error bound (epsilon)
         double probSuccess = 0.2;
         int iter = (int) Math.round(Math.log(epsilon) / Math.log(1 - probSuccess));
@@ -144,16 +145,27 @@ public class Program {
         double roundingFactor = 1 + delta;
         putils.printMessage("approximation factor is " + roundingFactor);
 
-        mainSeed = System.currentTimeMillis();
+
+        double bestScore = Double.MIN_VALUE;
+        long startTime = System.currentTimeMillis();
+        initComp(vertices);
         for (int i = 0; i < iter; ++i) {
-//        for (int i = 0; i < 1; ++i) {
-            runGraphComp(i, vertices);
+            putils.printMessage("  Start of Loop: " + i);
+            long loopStartTime = System.currentTimeMillis();
+            bestScore = Math.max(bestScore, runGraphComp(i, vertices));
+            putils.printMessage("  End of Loop: " + i + " took " + (System.currentTimeMillis() - loopStartTime) + " " +
+                    "ms");
         }
+        long endTime = System.currentTimeMillis();
+        putils.printMessage("Best score: " + bestScore);
+        putils.printMessage("\n== " + Constants.PROGRAM_NAME + " run ended on " + new Date() + " took " + (endTime -
+                startTime) + " ms ==\n");
     }
 
-    private static void runGraphComp(int loopNumber, Vertex[] vertices) throws MPIException {
-        initComp(vertices);
+    private static double runGraphComp(int loopNumber, Vertex[] vertices) throws MPIException {
+        initLoop(vertices);
 
+        long startTime = System.currentTimeMillis();
         for (int iter = 0; iter < twoRaisedToK; ++iter) {
             /* Super step loop*/
             int workerSteps = maxIterations+1; // +1 to send initial values
@@ -174,16 +186,14 @@ public class Program {
         ParallelOps.oneDoubleBuffer.put(0, bestScore);
         ParallelOps.worldProcsComm.allReduce(ParallelOps.oneDoubleBuffer, 1, MPI.DOUBLE, MPI.MAX);
         bestScore = ParallelOps.oneDoubleBuffer.get(0);
-        putils.printMessage("*** End of loop "  +loopNumber + " best score: " + bestScore);
+        putils.printMessage("    Loop "  +loopNumber + " best score: " + bestScore);
+        return bestScore;
     }
 
     private static double finalizeIterations(Vertex[] vertices) {
         double bestScore = Double.MIN_VALUE;
         for (Vertex vertex : vertices){
-            double score = vertex.finalizeIterations(alphaMax, roundingFactor);
-            if (score > bestScore){
-                bestScore = score;
-            }
+            bestScore = Math.max(bestScore, vertex.finalizeIterations(alphaMax, roundingFactor));
         }
         return bestScore;
     }
@@ -202,37 +212,20 @@ public class Program {
 
     private static void initComp(Vertex[] vertices) throws MPIException {
         roundingFactor =  1+delta;
-        Random random = new Random(mainSeed);
         // (1 << k) is 2 raised to the kth power
         twoRaisedToK = (1 << k);
-        int degree = (3 + Utils.log2(k));
-        gf = GaloisField.getInstance(1 << degree, Polynomial.createIrreducible(degree, random).toBigInteger().intValue());
         maxIterations = k-1; // the original pregel loop was from 2 to k (including k), so that's (k-2)+1 times
 
 
         int myDisplas = ParallelOps.localVertexDisplas[ParallelOps.worldProcRank];
         // same as vertices.length
         int myLength = ParallelOps.localVertexCounts[ParallelOps.worldProcRank];
-        for (int i = 0; i < globalVertexCount; ++i){
-            long uniqRandomVal = random.nextLong();
-            if (i >= myDisplas && i < myDisplas+myLength){
-                vertices[i-myDisplas].uniqueRandomSeed = uniqRandomVal;
-                // put vertex weights to be collected to compute maxweight - the sum of largest k weights
-                ParallelOps.vertexDoubleBuffer.put(i, vertices[i-myDisplas].weight);
-                vertices[i-myDisplas].weight
-                        = (int) Math.ceil(
-                        Utils.logb((int) vertices[i-myDisplas].weight + 1,
-                                roundingFactor));
-            }
+        for (int i = 0; i < myLength; ++i){
+            ParallelOps.vertexDoubleBuffer.put(myDisplas+i, vertices[i].weight);
+            vertices[i].weight = (int) Math.ceil(
+                    Utils.logb((int) vertices[i].weight + 1,
+                            roundingFactor));
         }
-
-        // DEBUG - check each vertex has a unique random seed
-        /*StringBuffer sb = new StringBuffer("Rank ");
-        sb.append(ParallelOps.worldProcRank).append('\n');
-        for (Vertex vertex : vertices){
-            sb.append(vertex.uniqueRandomSeed).append(" ");
-        }
-        System.out.println(sb.toString());*/
 
         ParallelOps.worldProcsComm.allGatherv(ParallelOps.vertexDoubleBuffer, ParallelOps.localVertexCounts,
                 ParallelOps.localVertexDisplas, MPI.DOUBLE);
@@ -256,11 +249,37 @@ public class Program {
         }
 
         r = (int) Math.ceil(Utils.logb((int) maxWeight + 1, roundingFactor));
-        putils.printMessage("Max Weight: " + maxWeight + " r: " + r);
+        putils.printMessage("  Max Weight: " + maxWeight + " r: " + r);
         // invalid input: r is negative
         if (r < 0) {
             throw new IllegalArgumentException("r must be a positive integer or 0");
         }
+    }
+
+    private static void initLoop(Vertex[] vertices) throws MPIException {
+        mainSeed = System.currentTimeMillis();
+        Random random = new Random(mainSeed);
+        int degree = (3 + Utils.log2(k));
+        gf = GaloisField.getInstance(1 << degree, Polynomial.createIrreducible(degree, random).toBigInteger().intValue());
+
+        int myDisplas = ParallelOps.localVertexDisplas[ParallelOps.worldProcRank];
+        // same as vertices.length
+        int myLength = ParallelOps.localVertexCounts[ParallelOps.worldProcRank];
+        for (int i = 0; i < globalVertexCount; ++i){
+            long uniqRandomVal = random.nextLong();
+            if (i >= myDisplas && i < myDisplas+myLength){
+                vertices[i-myDisplas].uniqueRandomSeed = uniqRandomVal;
+                // put vertex weights to be collected to compute maxweight - the sum of largest k weights
+            }
+        }
+
+        // DEBUG - check each vertex has a unique random seed
+        /*StringBuffer sb = new StringBuffer("Rank ");
+        sb.append(ParallelOps.worldProcRank).append('\n');
+        for (Vertex vertex : vertices){
+            sb.append(vertex.uniqueRandomSeed).append(" ");
+        }
+        System.out.println(sb.toString());*/
 
         randomAssignments = new TreeMap<>();
         ParallelOps.vertexLabelToWorldRank.keySet().forEach(
@@ -312,5 +331,55 @@ public class Program {
         cps = (cmd.hasOption(Constants.CMD_OPTION_SHORT_CPS)) ?
                 Integer.parseInt(cmd.getOptionValue(Constants.CMD_OPTION_SHORT_CPS)) :
                 -1;
+    }
+
+    public static String configPrettyString(boolean centerAligned) {
+        String[] params = {"Input File",
+                "Global Vertex Count",
+                "K",
+                "Epsilon",
+                "Delta",
+                "Alpha Max",
+                "Parallel Pattern",};
+        Object[] args =
+                new Object[]{inputFile,
+                        globalVertexCount,
+                        k,
+                        epsilon,
+                        delta,
+                        alphaMax,
+                        "1x"+(ParallelOps.worldProcsCount/ParallelOps.nodeCount)+"x"+ParallelOps.nodeCount,
+                        };
+
+        java.util.Optional<Integer> maxLength =
+                Arrays.stream(params).map(String::length).reduce(Math::max);
+        if (!maxLength.isPresent()) { return ""; }
+        final int max = maxLength.get();
+        final String prefix = "  ";
+        StringBuilder sb = new StringBuilder("Parameters...\n");
+        if (centerAligned) {
+            IntStream.range(0, params.length).forEach(
+                    i -> {
+                        String param = params[i];
+                        sb.append(getPadding(max - param.length(), prefix))
+                                .append(param).append(": ").append(args[i]).append('\n');
+                    });
+        }
+        else {
+            IntStream.range(0, params.length).forEach(
+                    i -> {
+                        String param = params[i];
+                        sb.append(prefix).append(param).append(':')
+                                .append(getPadding(max - param.length(), ""))
+                                .append(args[i]).append('\n');
+                    });
+        }
+        return sb.toString();
+    }
+
+    private static String getPadding(int count, String prefix){
+        StringBuilder sb = new StringBuilder(prefix);
+        IntStream.range(0,count).forEach(i -> sb.append(' '));
+        return sb.toString();
     }
 }
