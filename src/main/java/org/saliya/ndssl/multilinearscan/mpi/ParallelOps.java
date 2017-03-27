8978e5009427f4206560dab31b326ed605134d5b
Saliya Ekanayake <esaliya@gmail.com>
2017-03-27 13:12:32 -0400

    debugging parts

--- src/main/java/org/saliya/ndssl/multilinearscan/mpi/ParallelOps.java
+++ src/main/java/org/saliya/ndssl/multilinearscan/mpi/ParallelOps.java
@@ -88,9 +88,10 @@ public class ParallelOps {
         vertexIntBuffer = MPI.newIntBuffer(vertexCount);
         vertexLongBuffer = MPI.newLongBuffer(vertexCount);
         vertexDoubleBuffer = MPI.newDoubleBuffer(vertexCount);
-        return !Strings.isNullOrEmpty(partitionFile)
-                ? metisGraphPartition(file, partitionFile, vertexCount)
-                : simpleGraphPartition(file, vertexCount);
+        return metisGraphPartition(file, partitionFile, vertexCount);
+//        return !Strings.isNullOrEmpty(partitionFile)
+//                ? metisGraphPartition(file, partitionFile, vertexCount)
+//                : simpleGraphPartition(file, vertexCount);
     }
 
     private static Vertex[] metisGraphPartition(String graphFile, String partitionFile, int globalVertexCount) throws MPIException {
