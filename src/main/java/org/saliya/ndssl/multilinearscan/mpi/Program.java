acacfa4985d9285a50532ffc18c62dc10d99e817
Saliya Ekanayake <esaliya@gmail.com>
2017-03-30 16:53:01 -0400

    debugging

--- src/main/java/org/saliya/ndssl/multilinearscan/mpi/Program.java
+++ src/main/java/org/saliya/ndssl/multilinearscan/mpi/Program.java
@@ -131,6 +131,8 @@ public class Program {
     }
 
     private static void sendMessages(Vertex[] vertices, int superStep) throws MPIException {
+        System.out.println("--Rank: " + ParallelOps.worldProcRank + " came to program.sendMessages");
+
         int msgSize = -1;
 
         for (Vertex vertex : vertices){
